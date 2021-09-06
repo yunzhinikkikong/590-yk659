66499b8b5fed81a9f14698bf11b4516e5e15d152
yunzhinikkikong <yk659@georgetown.edu>
2021-09-05 21:10:16 -0400

    runall shell file

--- HW1.1/RUN_ALL.sh
+++ HW1.1/RUN_ALL.sh
@@ -1,7 +1,7 @@
 ScriptLoc=${PWD}
 cd LectureCodes
 for i in *.py; do echo $i; python $i; done
-grep “I HAVE WORKED” *
+grep "I HAVE WORKED" *
 cd $ScriptLoc
 for i in *.py; do echo $i; python $i; done
 
