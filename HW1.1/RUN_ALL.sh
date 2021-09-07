158f23867b80df6758246ef354f49478be18942b
yunzhinikkikong <yk659@georgetown.edu>
2021-09-06 22:38:47 -0400

    shell comment

--- HW1.1/RUN_ALL.sh
+++ HW1.1/RUN_ALL.sh
@@ -1,7 +1,7 @@
-ScriptLoc=${PWD}
+ScriptLoc=${PWD}    #save script directory path as shell variable
 cd LectureCodes
-for i in *.py; do echo $i; python $i; done
+for i in *.py; do echo $i; python $i; done   #run all python scripts in directory
 grep "I HAVE WORKED" *
 cd $ScriptLoc
-for i in *.py; do echo $i; python $i; done
+for i in *.py; do echo $i; python $i; done    #run all python scripts in directory
 
