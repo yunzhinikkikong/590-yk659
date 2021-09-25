0dedededc105dd237b66ec5ee66b593496ef5784
Yunzhi Kong <yk659@georgetown.edu>
2021-09-25 15:14:40 -0400

    textbook code

--- HW3.0/HW3.0A2.py
+++ HW3.0/HW3.0A2.py
@@ -70,6 +70,7 @@ import matplotlib.pyplot as plt
 history_dict = history.history
 loss_values = history_dict['loss']
 val_loss_values = history_dict['val_loss']
+acc = history.history['acc']
 epochs = range(1, len(acc) + 1)
 plt.plot(epochs, loss_values, 'bo', label='Training loss')
 plt.plot(epochs, val_loss_values, 'b', label='Validation loss')
