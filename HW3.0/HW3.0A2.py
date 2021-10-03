2f17c2a5b52ef9adb66c1bb34bce663d5f5601bc
Yunzhi Kong <yk659@georgetown.edu>
2021-10-03 11:00:53 -0400

    A2

--- HW3.0/HW3.0A2.py
+++ HW3.0/HW3.0A2.py
@@ -2,18 +2,17 @@
 #####Code Binary classification using KERAS, train on IMDB data set
 ########################################################################
 
+
+# Load IMDB dataset, the traning and testing set is defaulted
+# only keep top 10,000 most frequently occurring words 
+
 from keras.datasets import imdb
 (train_data, train_labels), (test_data, test_labels) = imdb.load_data(
      num_words=10000)
 
 
-max([max(sequence) for sequence in train_data])
 
-word_index = imdb.get_word_index()
-reverse_word_index = dict(
-      [(value, key) for (key, value) in word_index.items()])
-decoded_review = ' '.join(
-      [reverse_word_index.get(i - 3, '?') for i in train_data[0]])
+# vectorize the training and testing data
 
 import numpy as np
 def vectorize_sequences(sequences, dimension=10000):
@@ -28,52 +27,79 @@ x_test = vectorize_sequences(test_data)
 y_train = np.asarray(train_labels).astype('float32')
 y_test = np.asarray(test_labels).astype('float32')
 
+### Build the model, use a very small network with two hidden layers, each with 64 units
 
+
+############################################################
+############################################################
+
+# Here I explore different combinations of hyperparameter, 
+# and leave the best fit version in the below functions
+# I tried three 'relu' activation layers with different input units
+# and the out layer is sigmoid activation function that output range from 0 to 1
+# Actuallt, the textbook gives the best parameter combination that I have tried
+# I tried different activation function and layers, but the results are not as good as initial
+
+############################################################
+############################################################
 
 from keras import models
 from keras import layers
+
 model = models.Sequential()
-model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
-model.add(layers.Dense(16, activation='relu'))
+model.add(layers.Dense(32,activation='relu', input_shape=(10000,)))
+model.add(layers.Dense(16,activation='relu'))
+model.add(layers.Dense(16,activation='relu'))
 model.add(layers.Dense(1, activation='sigmoid'))
 
-model.compile(optimizer='rmsprop',
-    loss='binary_crossentropy', metrics=['accuracy'])
-
-from keras import optimizers
-model.compile(optimizer=optimizers.RMSprop(lr=0.001),
-loss='binary_crossentropy',
-metrics=['accuracy'])
-
-
-from keras import losses
-from keras import metrics
-model.compile(optimizer=optimizers.RMSprop(lr=0.001),
-loss=losses.binary_crossentropy,
-metrics=[metrics.binary_accuracy])
-
+# subseting the trainin dataset for a validation set
 
 x_val = x_train[:10000]
 partial_x_train = x_train[10000:]
 y_val = y_train[:10000]
 partial_y_train = y_train[10000:]
 
-model.compile(optimizer='rmsprop',loss='binary_crossentropy',metrics=['acc'])
+
+# Model compile
+
+model.compile(optimizer='rmsprop',
+              loss='binary_crossentropy',
+              metrics=['acc'])
 
 history = model.fit(partial_x_train,partial_y_train,epochs=20,
               batch_size=512,validation_data=(x_val, y_val))
 
+# save all fitting results in a dict
+
 history_dict = history.history
-history_dict.keys()
+
+
+############################################################
+############################################################
+
+# Model evaluation by results and plots
+# Plot for the training and validating loss
+# Plot for the training and validating accuracy
+# fit the test data and evaluate the loss and accuracy
+
+############################################################
+############################################################
+
 
 import matplotlib.pyplot as plt
-history_dict = history.history
+# save each keys value in the dict
 loss_values = history_dict['loss']
 val_loss_values = history_dict['val_loss']
 acc = history.history['acc']
+val_acc = history.history['val_acc']
 epochs = range(1, len(acc) + 1)
+
+
+
+# Plot for the training and validating loss
+
 plt.plot(epochs, loss_values, 'bo', label='Training loss')
-plt.plot(epochs, val_loss_values, 'b', label='Validation loss')
+plt.plot(epochs, val_loss_values, 'ro', label='Validation loss')
 plt.title('Training and validation loss')
 plt.xlabel('Epochs')
 plt.ylabel('Loss')
@@ -81,9 +107,24 @@ plt.legend()
 plt.show()
 
 
+# Plot for the training and validating accuracy
+
+plt.clf()
+acc_values = history_dict['acc']
+val_acc_values = history_dict['val_acc']
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'ro', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+
+
 model.fit(x_train, y_train, epochs=4, batch_size=512)
 results = model.evaluate(x_test, y_test)
-results
+print("Loss and Accuracy:",results)
 
 
 
