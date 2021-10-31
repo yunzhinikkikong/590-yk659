ee05b0f3f75a141fa8a1483dc3a62c99bb09fbd3
Yunzhi Kong <yk659@georgetown.edu>
2021-10-31 16:08:55 -0700

    training

--- HW5.0/02-train.py
+++ HW5.0/02-train.py
@@ -1,5 +1,6 @@
 import pickle
 import numpy as np
+import matplotlib.pyplot as plt
 
 # load the dataset
 
@@ -41,7 +42,7 @@ test_labels = to_categorical(test_labels)
 ############################################################
 
 
-
+### Borrow from lecture note
 #---------------------------
 #plotting function
 #---------------------------
@@ -100,7 +101,11 @@ model.compile(optimizer='rmsprop',
               metrics=['acc','AUC'])
 model.summary()
 history = model.fit(X_train, train_labels, epochs=10,batch_size=512, validation_data=(X_val,val_labels),verbose=verbose)
-#report(history,title="CNN")
+
+# # save the model
+model.save('1DCNN.h5')
+# # plot the results
+report(history,title="CNN")
 
 
 
@@ -120,7 +125,11 @@ model.compile(optimizer='rmsprop',
 model.summary()
 
 history = model.fit(X_train, train_labels, epochs=10,batch_size=512, validation_data=(X_val,val_labels),verbose=verbose)
-#report(history,title="SimpleRNN")
+
+# # plot the results
+report(history,title="SimpleRNN")
+# # save the model
+model.save('SimpleRNN.h5')
 
 
 
@@ -138,88 +147,3 @@ history = model.fit(X_train, train_labels, epochs=10,batch_size=512, validation_
 
 
 
-from keras import models
-from keras import layers
-model = models.Sequential()
-model.add(layers.Dense(64, activation='selu', input_shape=(1000,)))
-model.add(layers.Dense(64, activation='selu'))
-### multi-class output (46 mutually exclusive topics)
-model.add(layers.Dense(64, activation='selu'))
-model.add(layers.Dense(3, activation='softmax'))
-
-
-model.compile(optimizer='rmsprop',
-              loss='categorical_crossentropy',
-              metrics=['acc','AUC'])
-
-
-
-history = model.fit(X_train,
-                   one_hot_train_labels,
-                   epochs=40,
-                   batch_size=512,
-                   validation_data=(X_val, one_hot_val_labels))
-
-from keras.layers import Embedding, SimpleRNN
-from keras.layers import Dense
-model = models.Sequential()
-model.add(Embedding(1000, 32))
-model.add(SimpleRNN(32))
-model.add(Dense(3, activation='sigmoid'))
-model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
-
-history = model.fit(X_train,
-                   one_hot_train_labels,
-                   epochs=40,
-                   batch_size=512,
-                   validation_data=(X_val, one_hot_val_labels))
-
-
-
-import matplotlib.pyplot as plt
-loss = history.history['loss']
-val_loss = history.history['val_loss']
-acc = history.history['acc']
-val_acc = history.history['val_acc']
-auc = history.history['auc']
-val_auc = history.history['val_auc']
-epochs = range(1, len(loss) + 1)
-
-
-# Plot for the training and validating loss
-plt.plot(epochs, loss, 'bo', label='Training loss')
-plt.plot(epochs, val_loss, 'ro', label='Validation loss')
-plt.title('Training and validation loss')
-plt.xlabel('Epochs')
-plt.ylabel('Loss')
-plt.legend()
-plt.show()
-
-# Plot for the training and validating AUC
-
-plt.clf()
-plt.plot(epochs, auc, 'b', label='Training AUC')
-plt.plot(epochs, val_auc, 'r', label='Validation AUC')
-plt.title('Training and validation AUC')
-plt.xlabel('Epochs')
-plt.ylabel('AUC')
-plt.legend()
-plt.show()
-
-
-# Plot for the training and validating accuracy
-plt.clf()
-acc = history.history['acc']
-val_acc = history.history['val_acc']
-plt.plot(epochs, acc, 'bo', label='Training acc')
-plt.plot(epochs, val_acc, 'ro', label='Validation acc')
-plt.title('Training and validation accuracy')
-plt.xlabel('Epochs')
-plt.ylabel('Loss')
-plt.legend()
-plt.show()
-
-
-model.fit(X_train, one_hot_train_labels, epochs=20, batch_size=512)
-results = model.evaluate(X_test, one_hot_test_labels)
-print("Loss, Accuracy and AUC:",results)
