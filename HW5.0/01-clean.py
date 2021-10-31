0f5b079a5ebefc24e29f69b9f4a1040ba9478f11
Yunzhi Kong <yk659@georgetown.edu>
2021-10-31 15:59:02 -0700

    models

--- HW5.0/01-clean.py
+++ HW5.0/01-clean.py
@@ -5,9 +5,10 @@ import nltk
 import string
 nltk.download('punkt')
 
-Frankenstein = open('Frankenstein.txt')
-PrideandPrejudice = open('PrideandPrejudice.txt')
-Odyssey = open('The_Odyssey.txt')
+
+Frankenstein = open('/Users/nikkkikong/Desktop/Frankenstein.txt')
+PrideandPrejudice = open('/Users/nikkkikong/Desktop/PrideandPrejudice.txt')
+Odyssey = open('/Users/nikkkikong/Desktop/The_Odyssey.txt')
 
 def read_novel(f):
   # read .txt files
@@ -30,22 +31,25 @@ len(PrideandPrejudice_list)
 
 len(Odyssey_list)
 
+### add them together
+
 data_list = Frankenstein_list+PrideandPrejudice_list+Odyssey_list
 
 len(data_list)
 
-# Using Keras for word-level one-hot encoding
+# Using Keras for tokenization
 
 from keras.preprocessing.text import Tokenizer
-tokenizer = Tokenizer(num_words=1000)
+tokenizer = Tokenizer(num_words=10000)
 tokenizer.fit_on_texts(data_list)
 sequences = tokenizer.texts_to_sequences(data_list)
 one_hot_results = tokenizer.texts_to_matrix(data_list, mode='binary')
 word_index = tokenizer.word_index
 print('Found %s unique tokens.' % len(word_index))
+maxlen = 200 #CUTOFF REVIEWS maxlen 200 WORDS)
+from keras.preprocessing.sequence import pad_sequences
+data= pad_sequences(sequences,maxlen = maxlen)
 
-
-data_onehot=one_hot_results
 
 
 # labels: novel names
@@ -69,18 +73,18 @@ if(f_train+f_val+f_test != 1.0):
 	raise ValueError("f_train+f_val+f_test MUST EQUAL 1");
 
 #PARTITION DATA
-rand_indices = np.random.permutation(data_onehot.shape[0])
-CUT1=int(f_train*data_onehot.shape[0]); 
-CUT2=int((f_train+f_val)*data_onehot.shape[0]); 
+rand_indices = np.random.permutation(data.shape[0])
+CUT1=int(f_train*data.shape[0]); 
+CUT2=int((f_train+f_val)*data.shape[0]); 
 train_idx, val_idx, test_idx = rand_indices[:CUT1], rand_indices[CUT1:CUT2], rand_indices[CUT2:]
 print('------PARTITION INFO---------')
 print("train_idx shape:",train_idx.shape)
 print("val_idx shape:"  ,val_idx.shape)
 print("test_idx shape:" ,test_idx.shape)
 
-X_train = data_onehot[train_idx]
-X_val = data_onehot[val_idx]
-X_test = data_onehot[test_idx]
+X_train = data[train_idx]
+X_val = data[val_idx]
+X_test = data[test_idx]
 
 labels_train = labels[train_idx]
 labels_val = labels[val_idx]
@@ -92,4 +96,15 @@ dataset_dict = {"X_train": X_train, "X_test": X_test, "X_val": X_val, "train_lab
 
 with open('noveldataset_dict.pickle', 'wb') as file:
     pickle.dump(dataset_dict, file)
+
+exit()
+
+
+
+
+
+
+
+
+
 
--- HW5.0/02-train.py
+++ HW5.0/02-train.py
@@ -1,21 +1,28 @@
-
+import pickle
 import numpy as np
 
-with open('noveldataset_dict.pickle', 'rb') as file:
-    dataset_dict = pickle.load(file)
-### One-hot encoding for the training and testing labels
-### classify Reuters newswires into 3 mutually exclusive novels
+# load the dataset
 
-def to_one_hot(labels, dimension=3):
-    results = np.zeros((len(labels), dimension))
-    for i, label in enumerate(labels):
-        results[i, label] = 1.
-    return results
-one_hot_train_labels = to_one_hot(train_labels)
-one_hot_test_labels = to_one_hot(test_labels)
+with open('/Users/nikkkikong/590-yk659/HW5.0/noveldataset_dict.pickle', 'rb') as file:
+    dataset_dict = pickle.load(file)
+
+X_train = dataset_dict['X_train']
+X_val = dataset_dict['X_val']
+X_test = dataset_dict['X_test']
+
+train_labels = dataset_dict['train_labels']
+val_labels = dataset_dict['val_labels']
+test_labels = dataset_dict['test_labels']
+
+# make the lables as catogorical variables with three classes:
+# 0: Frankenstein
+# 1: PrideandPrejudice
+# 2: Odyssey
 from keras.utils.np_utils import to_categorical
-one_hot_train_labels = to_categorical(train_labels)
-one_hot_test_labels = to_categorical(test_labels)
+train_labels = to_categorical(train_labels)
+val_labels = to_categorical(val_labels)
+test_labels = to_categorical(test_labels)
+
 
 ############################################################
 ############################################################
@@ -25,24 +32,120 @@ one_hot_test_labels = to_categorical(test_labels)
 ### Multi-class single-label classification: softmax output, 
 # Loss: categorical_crossentropy
 
-# Here I explore different combinations of hyperparameter, 
-# and leave the best fit version in the below functions
-# I tried three 'selu' activation layers with different 64 input units
-# 'selu' is one of the Relu alternative
-# and the out layer is softmax activation function since it is a Multi-class single-label classification quesion
-# The validation/training loss are lower than textbook example
+### 1D CNN model
+
+### RNN model, here I will use SimpleRNN
+
 
 ############################################################
 ############################################################
+
+
+
+#---------------------------
+#plotting function
+#---------------------------
+def report(history,title='',I_PLOT=True):
+
+    print(title+": TEST METRIC (loss,accuracy,AUC):",model.evaluate(X_test,test_labels,batch_size=512,verbose=1))
+
+    if(I_PLOT):
+        #PLOT HISTORY
+        epochs = range(1, len(history.history['loss']) + 1)
+        plt.figure()
+        plt.plot(epochs, history.history['loss'], 'bo', label='Training loss')
+        plt.plot(epochs, history.history['val_loss'], 'b', label='Validation loss')
+
+        plt.plot(epochs, history.history['acc'], 'ro', label='Training acc')
+        plt.plot(epochs, history.history['val_acc'], 'r', label='Validation acc')
+        
+        plt.plot(epochs, history.history['auc'], 'go', label='Training auc')
+        plt.plot(epochs, history.history['val_auc'], 'g', label='Validation auc')
+
+        plt.title(title)
+        plt.legend()
+        # plt.show()
+
+        plt.savefig('HISTORY'+title+'.png')   # save the figure to file
+        plt.close()
+        
+
+
+
+
+from keras import layers
+from keras.models import Sequential
+from keras.layers import Embedding, Flatten, Dense
+from keras.layers import SimpleRNN
+
+
+verbose = 1
+embedding_dim = 128 #DIMENSION OF EMBEDING SPACE (SIZE OF VECTOR FOR EACH WORD)
+max_words = 10000  #DEFINES SIZE OF VOCBULARY TO USE
+maxlen = 200 #CUTOFF REVIEWS maxlen 200 WORDS)
+
+print("---------------------------")
+print("1D-CNN")  
+print("---------------------------")
+
+model = Sequential()
+model.add(layers.Embedding(max_words, embedding_dim, input_length=maxlen))
+model.add(layers.Conv1D(64, 7, activation='relu'))
+model.add(layers.MaxPooling1D(5))
+model.add(layers.Conv1D(64, 7, activation='relu'))
+model.add(layers.GlobalMaxPooling1D())
+model.add(layers.Dense(3))
+model.compile(optimizer='rmsprop',
+              loss='categorical_crossentropy',
+              metrics=['acc','AUC'])
+model.summary()
+history = model.fit(X_train, train_labels, epochs=10,batch_size=512, validation_data=(X_val,val_labels),verbose=verbose)
+#report(history,title="CNN")
+
+
+
+
+print("---------------------------")
+print("SimpleRNN")  
+print("---------------------------")
+
+
+model = Sequential() 
+model.add(layers.Embedding(max_words, embedding_dim, input_length=maxlen))
+model.add(layers.SimpleRNN(32)) 
+model.add(layers.Dense(3, activation='softmax'))
+model.compile(optimizer='rmsprop',
+              loss='categorical_crossentropy',
+              metrics=['acc','AUC'])
+model.summary()
+
+history = model.fit(X_train, train_labels, epochs=10,batch_size=512, validation_data=(X_val,val_labels),verbose=verbose)
+#report(history,title="SimpleRNN")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 from keras import models
 from keras import layers
 model = models.Sequential()
-model.add(layers.Dense(64, activation='selu', input_shape=(10000,)))
+model.add(layers.Dense(64, activation='selu', input_shape=(1000,)))
 model.add(layers.Dense(64, activation='selu'))
 ### multi-class output (46 mutually exclusive topics)
 model.add(layers.Dense(64, activation='selu'))
-model.add(layers.Dense(46, activation='softmax'))
+model.add(layers.Dense(3, activation='softmax'))
 
 
 model.compile(optimizer='rmsprop',
@@ -50,18 +153,73 @@ model.compile(optimizer='rmsprop',
               metrics=['acc','AUC'])
 
 
-# subseting the trainin dataset for a validation set
 
-
-x_val = x_train[:1000]
-partial_x_train = x_train[1000:]
-y_val = one_hot_train_labels[:1000]
-partial_y_train = one_hot_train_labels[1000:]
-
-
-
-history = model.fit(partial_x_train,
-                   partial_y_train,
+history = model.fit(X_train,
+                   one_hot_train_labels,
                    epochs=40,
                    batch_size=512,
-                   validation_data=(x_val, y_val))
+                   validation_data=(X_val, one_hot_val_labels))
+
+from keras.layers import Embedding, SimpleRNN
+from keras.layers import Dense
+model = models.Sequential()
+model.add(Embedding(1000, 32))
+model.add(SimpleRNN(32))
+model.add(Dense(3, activation='sigmoid'))
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
+
+history = model.fit(X_train,
+                   one_hot_train_labels,
+                   epochs=40,
+                   batch_size=512,
+                   validation_data=(X_val, one_hot_val_labels))
+
+
+
+import matplotlib.pyplot as plt
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+auc = history.history['auc']
+val_auc = history.history['val_auc']
+epochs = range(1, len(loss) + 1)
+
+
+# Plot for the training and validating loss
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'ro', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+# Plot for the training and validating AUC
+
+plt.clf()
+plt.plot(epochs, auc, 'b', label='Training AUC')
+plt.plot(epochs, val_auc, 'r', label='Validation AUC')
+plt.title('Training and validation AUC')
+plt.xlabel('Epochs')
+plt.ylabel('AUC')
+plt.legend()
+plt.show()
+
+
+# Plot for the training and validating accuracy
+plt.clf()
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'ro', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+
+model.fit(X_train, one_hot_train_labels, epochs=20, batch_size=512)
+results = model.evaluate(X_test, one_hot_test_labels)
+print("Loss, Accuracy and AUC:",results)
