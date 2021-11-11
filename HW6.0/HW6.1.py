ae6cb5202f99dca1ff636f1460d82c9f55edc5e8
Yunzhi Kong <yk659@georgetown.edu>
2021-11-10 19:36:59 -0500

    1 and 2

--- HW6.0/HW6.1.py
+++ HW6.0/HW6.1.py
@@ -26,17 +26,45 @@ model.add(layers.Dense(28*28,  activation='relu'))
 model.summary()
 
 
-
 #COMPILE AND FIT
 model.compile(optimizer='rmsprop',
                 loss='mean_squared_error')
 
-# define error threshold
 
 
 history = model.fit(X, X, epochs=10, batch_size=500,validation_split=0.2)
 
+# define error threshold
+
 threshold = 4 * model.evaluate(X,X,batch_size=X.shape[0])
+
+# anomaly detection
+# error > threshold == > anomaly
+
+# 1 = anomaly, 0 = normal
+X1=model.predict(X) 
+errors = losses.mse(X1, X)
+anomaly = pd.Series(errors) > threshold
+preds = anomaly.map(lambda x: 1.0 if x == True else 0)
+
+print("anomaly fraction (“trained” data):", preds.sum()/X.shape[0],"\n","anomaly count(“trained” data):",preds.sum())
+
+
+# Plot original and RECONSTRUCTED 
+
+#RESHAPE
+X=X.reshape(60000,28,28); #print(X[0])
+X1=X1.reshape(60000,28,28); #print(X[0])
+
+
+f, ax = plt.subplots(4,1)
+I1=11; I2=46
+ax[0].imshow(X[I1])
+ax[1].imshow(X1[I1])
+ax[2].imshow(X[I2])
+ax[3].imshow(X1[I2])
+plt.show()
+
 
 #PLOT HISTORY
 epochs = range(1, len(history.history['loss']) + 1)
@@ -50,23 +78,7 @@ plt.show()
 
 
 
-#PLOT ORIGINAL AND RECONSTRUCTED 
-X1=model.predict(X) 
-errors = losses.mse(X1, X)
 
-# 1 = anomaly, 0 = normal
-anomaly = pd.Series(errors) > threshold
-preds = anomaly.map(lambda x: 1.0 if x == True else 0)
-
-print("anomaly fraction (“trained” data):", preds.sum()/X.shape[0])
-
-#RESHAPE
-X=X.reshape(60000,28,28); #print(X[0])
-X1=X1.reshape(60000,28,28); #print(X[0])
-
-#COMPARE ORIGINAL 
-
-model.evaluate()
 # import fashion_mnist dataset
 
 from keras.datasets import fashion_mnist
@@ -86,14 +98,11 @@ anomaly_mask = pd.Series(errors) > threshold
 preds = anomaly_mask.map(lambda x: 1.0 if x == True else 0)
 
 
-print("anomaly fraction (“anomalies” data):", preds.sum()/x.shape[0])
+print("anomaly fraction (“anomalies” data):", preds.sum()/x.shape[0],"\n","anomaly count(“anomalies” data):", preds.sum())
 
-model.evaluate(x,x,batch_size=x.shape[0])
-#history_f = model.fit(x, x, epochs=10, batch_size=500)
-#error = history_f.history['loss']
 
-#x1.history = model.fit(X, X, epochs=10, batch_size=1000,validation_split=0.2)
-#model.evaluate(x1,x1,batch_size=x1.shape[0])
+
+#PLOT ORIGINAL AND RECONSTRUCTED 
 
 x=x.reshape(60000,28,28); #print(X[0])
 x1=x1.reshape(60000,28,28);
@@ -107,4 +116,9 @@ ax[3].imshow(x1[I2])
 plt.show()
 
 
+#model.evaluate(x,x,batch_size=x.shape[0])
+#history_f = model.fit(x, x, epochs=10, batch_size=500)
+#error = history_f.history['loss']
 
+#x1.history = model.fit(X, X, epochs=10, batch_size=1000,validation_split=0.2)
+#model.evaluate(x1,x1,batch_size=x1.shape[0])
--- HW6.0/WH6.2.py
+++ HW6.0/WH6.2.py
@@ -5,7 +5,7 @@
 import numpy as np
 import pandas as pd
 import seaborn as sns
-
+from keras import losses
 import matplotlib.pyplot as plt
 from keras import models
 from keras import layers
@@ -51,32 +51,33 @@ model.summary()
 model.compile(optimizer='rmsprop',
                 loss='mean_squared_error')
 
-# define error threshold
-#threshold = 4 * model.evaluate(X,X,batch_size=X.shape[0])
+
 
 history = model.fit(X, X, epochs=10, batch_size=500,validation_split=0.2)
 
+# define error threshold
 
+threshold = 4 * model.evaluate(X,X,batch_size=X.shape[0])
 
-#PLOT HISTORY
-epochs = range(1, len(history.history['loss']) + 1)
-plt.figure()
-plt.plot(epochs, history.history['loss'], 'bo', label='Training loss')
-plt.plot(epochs, history.history['val_loss'], 'b', label='Validation loss')
+# anomaly detection
+# error > threshold == > anomaly
 
-plt.title("MNIST-- deep feed forward AE")
-plt.legend()
-plt.show()
-
-
-
-#PLOT ORIGINAL AND RECONSTRUCTED 
+# 1 = anomaly, 0 = normal
 X1=model.predict(X) 
+X=X.reshape(60000,28*28); #print(X[0])
+X1=X1.reshape(60000,28*28);
+errors = losses.mse(X1, X)
+anomaly = pd.Series(errors) > threshold
+preds = anomaly.map(lambda x: 1.0 if x == True else 0)
+
+print("anomaly fraction (“trained” data):", preds.sum()/X.shape[0],"\n","anomaly count(“trained” data):",preds.sum())
+
+
+# Plot original and RECONSTRUCTED 
 
 #RESHAPE
 X=X.reshape(60000,28,28); #print(X[0])
 X1=X1.reshape(60000,28,28); #print(X[0])
-
 
 #COMPARE ORIGINAL 
 f, ax = plt.subplots(4,1)
@@ -87,10 +88,15 @@ ax[2].imshow(X[I2])
 ax[3].imshow(X1[I2])
 plt.show()
 
+#PLOT HISTORY
+epochs = range(1, len(history.history['loss']) + 1)
+plt.figure()
+plt.plot(epochs, history.history['loss'], 'bo', label='Training loss')
+plt.plot(epochs, history.history['val_loss'], 'b', label='Validation loss')
 
-
-
-#COMPARE ORIGINAL 
+plt.title("MNIST-- Convolutional AE")
+plt.legend()
+plt.show()
 
 
 # import fashion_mnist dataset
@@ -102,10 +108,20 @@ from keras.datasets import fashion_mnist
 #NORMALIZE AND RESHAPE
 x=x/np.max(x) 
 x=x.reshape(60000,28,28,1); 
-x1= model.predict(x) 
 
-#x1.history = model.fit(X, X, epochs=10, batch_size=1000,validation_split=0.2)
-#model.evaluate(x1,x1,batch_size=x1.shape[0])
+x1= model.predict(x) 
+x=x.reshape(60000,28*28); #print(X[0])
+x1=x1.reshape(60000,28*28);
+errors = losses.mse(x1, x)
+
+# 1 = anomaly, 0 = normal
+anomaly_mask = pd.Series(errors) > threshold
+preds = anomaly_mask.map(lambda x: 1.0 if x == True else 0)
+
+
+print("anomaly fraction (“anomalies” data):", preds.sum()/x.shape[0],"\n","anomaly count(“anomalies” data):", preds.sum())
+
+
 
 x=x.reshape(60000,28,28); #print(X[0])
 x1=x1.reshape(60000,28,28);
